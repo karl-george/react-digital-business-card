8bed163e6a54bfa1de761d307f45d877be8aebdd
Karl George <karlgdev@gmail.com>
2022-03-22 10:13:27 +0100

    Initial commit

---
+++ src/Info.js
@@ -0,0 +1,16 @@
+import '../Info.css'
+
+
+const Info = () => {
+  return (
+    <header>
+      <img src="" alt="" />
+      <h1></h1>
+      <h3>Karl George</h3>
+      <p>Frontend Developer</p>
+      <button></button>
+    </header>
+  )
+}
+
+export default Info
